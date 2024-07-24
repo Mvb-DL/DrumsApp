0b5f70be2c2e6528b75d00066682b12094bd788c
Mario.v.B <67900070+Mvb-DL@users.noreply.github.com>
2024-07-24 14:31:10 +0200

    added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
 // app/layout.js
 import './globals.scss';
+import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'My Next App',
   description: 'A Next.js project with Tailwind CSS and SCSS',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
